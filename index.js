0939b791a2680291e93aef9cb884fe4691831c14
ydivya01 <ydivya01@gmail.com>
2023-09-02 03:21:45 +0530

    cors error

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ dotenv.config();
 app.use(express.json())
 
 app.use(cors({
-    origin:["http://localhost:3000"],
+    // origin:["http://localhost:3000"],
+    origin:["https://shimmering-kheer-34c615.netlify.app"],
     methods:["GET", "POST", "PUT", "DELETE"],
     credentials:true,
     optionSuccessStatus:200
