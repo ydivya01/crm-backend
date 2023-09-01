0edfe70cd88ecf0cf4ed41edc1bebe5a236423f3
ydivya01 <ydivya01@gmail.com>
2023-09-01 23:08:33 +0530

    npm

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.use(express.json())
 app.use(cors({
     origin:["http://localhost:3000"],
     methods:["GET", "POST", "PUT", "DELETE"],
-    credentials:true
+    credentials:true,
+    optionSuccessStatus:200
 }))
 
 app.use(cookieParser())
